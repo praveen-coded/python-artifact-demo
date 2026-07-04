739d33c174f2c60ab7ef58592491ac1be0b41f77
praveen-coded <109722723+praveen-coded@users.noreply.github.com>
2026-07-04 20:09:03 +0530

    Rename output file from result.txt to results.txt

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 print("Python artifact demo")
 
-with open("result.txt", "w") as file:
+with open("results.txt", "w") as file:
   file.write("Hello from GitHub\n")
   file.write("This file is created in Git\n")
   file.write(f"Genaretd Time: {datetime.now()}\n")
